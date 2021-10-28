8663421b94122922d365818b8977f442f1db1375
Richard Otte <rotte@blackducksoftware.com>
2021-10-28 11:28:40 -0400

    initial commit

---
+++ src/main/java/com/synopsys/centralintegrations/blackduck/bdio/BdioUpload.java
@@ -0,0 +1,72 @@
+package com.synopsys.centralintegrations.blackduck.bdio;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.synopsys.integration.blackduck.codelocation.bdio2legacy.Bdio2UploadService;
+import com.synopsys.integration.blackduck.codelocation.upload.UploadBatch;
+import com.synopsys.integration.blackduck.codelocation.upload.UploadTarget;
+import com.synopsys.integration.blackduck.configuration.BlackDuckServerConfig;
+import com.synopsys.integration.blackduck.service.BlackDuckServicesFactory;
+import com.synopsys.integration.log.SilentIntLogger;
+import com.synopsys.integration.util.NameVersion;
+
+@SpringBootApplication
+public class BdioUpload {
+    public static void main(String[] args) throws Exception {
+        Path stateJsonFilePath = Paths.get(args[0]);
+        Gson gson = new Gson();
+
+        String blackDuckAuthToken;
+        String blackDuckInstanceUrl;
+        String blackDuckProjectName;
+        String bdioPath;
+        try (BufferedReader reader = Files.newBufferedReader(stateJsonFilePath)){
+            JsonObject stateJson = gson.fromJson(reader, JsonObject.class);
+
+            JsonObject blackDuckStateJson = stateJson.getAsJsonObject("Blackduck");
+
+            blackDuckAuthToken = blackDuckStateJson
+                .get("AuthToken")
+                .getAsString();
+
+            blackDuckInstanceUrl = blackDuckStateJson
+                .get("InstanceUrl")
+                .getAsString();
+
+            blackDuckProjectName = blackDuckStateJson
+                .get("ProjectName")
+                .getAsString();
+
+            bdioPath = stateJson
+                .getAsJsonObject("Resources")
+                .getAsJsonObject("Com")
+                .getAsJsonObject("Synopsys")
+                .getAsJsonObject("CentralIntegrations")
+                .getAsJsonObject("BlackDuck")
+                .getAsJsonObject("Bdio")
+                .get("BdioPath")
+                .getAsString();
+        }
+
+        BlackDuckServerConfig blackDuckServerConfig = BlackDuckServerConfig.newBuilder()
+                                                          .setUrl(blackDuckInstanceUrl)
+                                                          .setApiToken(blackDuckAuthToken)
+                                                          .buildWithoutValidation();
+
+        BlackDuckServicesFactory blackDuckServicesFactory = blackDuckServerConfig.createBlackDuckServicesFactory(new SilentIntLogger());
+
+        Bdio2UploadService bdio2UploadService = blackDuckServicesFactory.createBdio2UploadService();
+
+        NameVersion nameVersion = new NameVersion(blackDuckProjectName.split(":")[0], blackDuckProjectName.split(":")[1]);
+
+        bdio2UploadService.uploadBdio(new UploadBatch(UploadTarget.createDefault(nameVersion, "IO", new File(bdioPath))));
+    }
+}
